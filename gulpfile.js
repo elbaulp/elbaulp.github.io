524305060e0f357da061edccbba3866f0a10049d
Alejandro Alcalde <algui91@gmail.com>
2017-01-04 18:42:32 +0100

    Added amp-sidebar

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,20 @@
 /* Refrences:
-1. http://notes.iissnan.com/2016/publishing-github-pages-with-travis-ci
-2. https://github.com/chrisjlee/hexo-theme-zurb-foundation/blob/e82f45a82bbaaee063bcb1298cd9793575afb142/gulpfile.js
-3. https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
-4. https://hexo.io/api/
-5. https://github.com/iissnan/theme-next-docs/blob/master/.travis.yml
+   1. http://notes.iissnan.com/2016/publishing-github-pages-with-travis-ci
+   2. https://github.com/chrisjlee/hexo-theme-zurb-foundation/blob/e82f45a82bbaaee063bcb1298cd9793575afb142/gulpfile.js
+   3. https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
+   4. https://hexo.io/api/
+   5. https://github.com/iissnan/theme-next-docs/blob/master/.travis.yml
 */
 
-var gulp = require('gulp');
-var minifycss = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
-var imagemin = require('gulp-imagemin');
-var del = require('del');
+var gulp        = require('gulp');
+var minifycss   = require('gulp-clean-css');
+var uglify      = require('gulp-uglify');
+var htmlmin     = require('gulp-htmlmin');
+var htmlclean   = require('gulp-htmlclean');
+var imagemin    = require('gulp-imagemin');
+var del         = require('del');
 var runSequence = require('run-sequence');
-var koutoSwiss  = require('kouto-swiss');
-var Hexo = require('hexo');
-var stylus = require('gulp-stylus');
-var prefixer    = require('autoprefixer-stylus');
-var jeet        = require('jeet');
+var Hexo        = require('hexo');
 
 gulp.task('clean', function() {
     return del(['public/**/*']);
@@ -40,7 +36,7 @@ gulp.task('generate', function(cb) {
         hexo.exit(err);
         return cb(err);
     })
-});
+        });
 
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
@@ -77,7 +73,6 @@ gulp.task('minify-img', function() {
 gulp.task('compress', function(cb) {
     runSequence(['minify-html', 'minify-css', 'minify-js', 'minify-img'], cb);
 });
-
 
 //gulp.task('build', ['clean', 'generate', 'compress']);
 gulp.task('build', function(cb) {
