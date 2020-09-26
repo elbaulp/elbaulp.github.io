69c691b1ee094c4ff2b8251a6c98fab23ddaa644
Alejandro Alcalde <alejandro.alcalde@moia.io>
2020-09-26 07:26:55 +0200

    Minify htmlcode

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM iquiw/alpine-emacs:latest
 
 WORKDIR /root
 
+RUN apk update && apk add --update npm
+RUN npm install html-minifier-terser -g
 COPY projects.lisp index.org build.sh ./
 COPY static/org-templates static/org-templates
 COPY static/hugo static/hugo
--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ mkdir -p ~/.emacs.d/org-templates
 cp -Rv /root/static/org-templates/* ~/.emacs.d/org-templates/
 emacs --batch --no-init-file --load ./projects.lisp --funcall toggle-debug-on-error
 cp -R /root/static/hugo/* public/
+html-minifier-terser
+html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./public --output-dir ./public --file-ext html
