07381532a8cb7e2d8e8667b486ab5be9b14dc42d
Alejandro Alcalde <alejandro.alcalde@moia.io>
2020-09-26 08:02:23 +0200

    Add css for icons

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cp -Rv /root/static/org-templates/* ~/.emacs.d/org-templates/
 emacs --batch --no-init-file --load ./projects.lisp --funcall toggle-debug-on-error
 cp -R /root/static/hugo/* public/
 html-minifier-terser
-html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./public --output-dir ./public --file-ext html
+html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./public --output-dir ./public --file-ext html --file-ext css
