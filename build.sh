46738ac9f28deb21817dc107d93618e991c8d43d
Alejandro Alcalde <alejandro.alcalde@moia.io>
2020-10-03 19:58:52 +0200

    Change directory to public

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+cd /root/blog/
+
 html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./public/ --output-dir ./public --file-ext html
 html-minifier-terser --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./public/ --output-dir ./public --file-ext css
 mv ./static/hugo/* ./public
