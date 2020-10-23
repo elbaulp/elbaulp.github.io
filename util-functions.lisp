8c56e542514499b32af3408d1028765ec888dbab
Alejandro Alcalde <alejandro.alcalde@moia.io>
2020-10-23 09:58:01 +0200

    Enable info-js, code refactor, closes #40

--- util-functions.lisp
+++ util-functions.lisp
@@ -1,4 +1,6 @@
-;; Functions
+;;;;;;;;;;;;;;;;;;;;;;;
+;; Misc Functions    ;;
+;;;;;;;;;;;;;;;;;;;;;;;
 (defun elbaul/filter-path (path list)
   "Return sublist containing path as parent"
   (cdr
@@ -16,6 +18,18 @@
   (replace-regexp-in-string (regexp-quote old) new s t t))
 
 
+(defun get-string-from-file (filePath)
+  "Return filePath's file content.
+  (http://ergoemacs.org/emacs/elisp_read_file_content.html)"
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;
+;; Sitemap Functions ;;
+;;;;;;;;;;;;;;;;;;;;;;;
 (defun duncan/latest-posts-sitemap-function (title sitemap)
   "posts.org generation. Only publish the latest 5 posts from SITEMAP (https://orgmode.org/manual/Sitemap.html).  Skips TITLE."
   (let* ((posts (cdr sitemap))
@@ -45,10 +59,41 @@
             (org-list-to-org (cons (car sitemap) posts))
             )))
 
+;;;;;;;;;;;;;;;;;;;
+;; RSS functions ;;
+;;;;;;;;;;;;;;;;;;;
+(defun duncan/sitemap-for-rss-sitemap-function (title sitemap)
+  "Publish rss.org which needs each entry as a headline."
+  (let* ((title "Blog") (subtitle "Archive")
+         (posts (cdr sitemap))
+         (posts (duncan/org-publish-sitemap--valid-entries posts)))
+    (concat (format "#+TITLE: %s\n\n" title)
+            (org-list-to-generic
+             posts
+             (org-combine-plists
+              (list :splice t
+                    :istart nil
+                    :icount nil
+                    :dtstart " " :dtend " "))))))
 
-(defun get-string-from-file (filePath)
-  "Return filePath's file content.
-  (http://ergoemacs.org/emacs/elisp_read_file_content.html)"
-  (with-temp-buffer
-    (insert-file-contents filePath)
-    (buffer-string)))
+
+(defun duncan/sitemap-for-rss-sitemap-format-entry (entry style project)
+  "Format ENTRY for rss.org for excusive use of exporting to RSS/XML. Each entry needs to be a headline. STYLE is not used."
+  (let* ((base-directory (plist-get (cdr project) :base-directory))
+         (filename (expand-file-name entry (expand-file-name base-directory ".")))
+
+         (title (duncan/post-get-metadata-from-frontmatter filename "TITLE"))
+         ;;(title (org-publish-format-file-entry "%t" filename project))
+         ;;(title (org-publish-find-title filename project))
+         (date (format-time-string "<%Y-%m-%d>" (org-publish-find-date entry project)))
+         (draft? (duncan/post-get-metadata-from-frontmatter filename "DRAFT")))
+    (unless (or (equal entry "404.org") draft?)
+      (with-temp-buffer
+             (insert (format "* [[file:%s][%s]]\n" entry title))
+             (org-set-property "RSS_PERMALINK" (concat "posts/" (file-name-sans-extension entry) ".html"))
+             (org-set-property "RSS_TITLE" title)
+             (org-set-property "PUBDATE" date)
+             ;; to avoid second update to rss.org by org-icalendar-create-uid
+             ;;(insert-file-contents entry)
+             (buffer-string))))
+              )
